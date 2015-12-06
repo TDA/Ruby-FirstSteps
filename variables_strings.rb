23ddbe3ce9ce9ac8f7820d57133f9cf75e065d62
Sai Pc <schand31@asu.edu>
2015-12-06 13:56:53 -0700

    strings, basic formatting

---
+++ variables_strings.rb
@@ -0,0 +1,8 @@
+class Variables_Strings
+   hello = "sss"
+   puts hello, "\n\n" # puts seems to put a newline after the sentence by def
+   print hello.upcase # print doesnt
+   puts
+   hello = hello.upcase
+   puts hello
+end
