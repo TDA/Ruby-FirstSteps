508acc6f5079d1b7a898956897b887ca4e47bdd4
Sai Pc <schand31@asu.edu>
2015-12-06 19:54:19 -0700

    finished the game, had to only figure out how to take in integers from cmd line :D

--- game_prompt.rb
+++ game_prompt.rb
@@ -6,7 +6,7 @@ class GamePrompt
   no_of_guesses = 0
   max_no_of_guesses = 7
   puts "Guess the number"
-  guess = gets.chomp
+  guess = gets.chomp.to_i
   p guess
   while guess != random
     if (guess < random) then
@@ -16,12 +16,13 @@ class GamePrompt
     end
 
     no_of_guesses += 1
+    puts "You have #{max_no_of_guesses - no_of_guesses} left"
+    # puts "You have", max_no_of_guesses - no_of_guesses, "left"
     if no_of_guesses >= max_no_of_guesses
       puts "too bad, you couldnt guess my number", random
       break
     end
-
-
+    guess = gets.chomp.to_i
   end
   if (no_of_guesses < max_no_of_guesses) then
     puts "Congrats you got it right"
