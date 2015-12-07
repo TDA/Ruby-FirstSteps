4cf77f704e9f4e787897ed8f305174cf68970e2f
Sai Pc <schand31@asu.edu>
2015-12-06 20:20:35 -0700

    fixed quirks and pushed

--- game_prompt.rb
+++ game_prompt.rb
@@ -1,31 +1,30 @@
 class GamePrompt
   puts "Enter your name"
-  name = gets
+  name = gets.chomp
   puts "Welcome #{name}"
   random = rand(100) + 1
-  no_of_guesses = 0
+  no_of_guesses = 1
   max_no_of_guesses = 7
   puts "Guess the number"
   guess = gets.chomp.to_i
-  p guess
-  while guess != random
-    if (guess < random) then
+  guessed_right = false
+  until guessed_right || no_of_guesses > max_no_of_guesses
+    if guess == random
+      guessed_right = true
+      break
+    end
+    if guess < random
       puts "Too low"
     else
       puts "Too high"
     end
-
-    no_of_guesses += 1
     puts "You have #{max_no_of_guesses - no_of_guesses} left"
-    # puts "You have", max_no_of_guesses - no_of_guesses, "left"
-    if no_of_guesses >= max_no_of_guesses
-      puts "Too bad, you couldnt guess my number", random
-      break
-    end
+    no_of_guesses += 1
     guess = gets.chomp.to_i
   end
-  if (no_of_guesses < max_no_of_guesses) then
-    puts "Congrats you got it right"
+  if guessed_right
+    puts "Congrats #{name} you got it right in #{no_of_guesses} guesses"
+  else
+    puts "Too bad, you couldnt guess my number", random
   end
-
 end
