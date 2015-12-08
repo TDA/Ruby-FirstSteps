262ad23afa61b215ef861b0c047ef18f0b13e7fb
Sai Pc <schand31@asu.edu>
2015-12-07 23:16:45 -0700

    miller indices

---
+++ miller_indices.rb
@@ -0,0 +1,17 @@
+class MillerIndices
+  print "x = "
+  x = gets.to_f
+  puts x
+  print "y = "
+  y = gets.to_f
+  puts y
+  print "z = "
+  z = gets.to_f
+  puts z
+
+  reci_x = 1.0 / x
+  reci_y = 1.0 / y
+  reci_z = 1.0 / z
+
+  puts reci_x, reci_y, reci_z
+end
