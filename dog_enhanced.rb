41677d44f3da7a0114adb7cb093d3d664ecb83b7
Sai Pc <schand31@asu.edu>
2015-12-15 14:20:47 -0700

    validation and more object orientation

--- dog_enhanced.rb
+++ dog_enhanced.rb
@@ -1,9 +1,34 @@
 class DogEnhanced
-  attr_accessor :age, :name
+  attr_reader :age, :name
+  # adding validation
+  def name=(name)
+    if name == ""
+      raise "Name cant be empty mister"
+    else
+      @name = name
+    end
+  end
+
+  def age=(age)
+    if age < 0
+      raise "Age cant be #{age} mister"
+    else
+      @age = age
+    end
+  end
+
 end
 
 dog = DogEnhanced.new
 dog.name = "rookie"
 dog.age = 1
+puts "#{dog.name} name"
+puts "#{dog.age} age"
+
+
+# Looks like u can redefine/reuse the same object :\
+dog = DogEnhanced.new
+dog.name = "as"
+dog.age = 0
 puts "#{dog.name} name"
 puts "#{dog.age} age"
