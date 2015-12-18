3270f891b13d8a39d467d7cdd488709007cb15e3
Sai Pc <schand31@asu.edu>
2015-12-18 16:40:20 +0530

    arrays

---
+++ array_ops.rb
@@ -0,0 +1,15 @@
+array = ["a", "b", "c", "d"]
+p array
+puts array.shift
+p array
+puts array.pop
+p array
+puts array.join
+p array
+
+array = [1, 2, 3, 4]
+sum = 0
+for a in array
+  sum += a
+end
+puts sum
