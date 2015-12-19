b14438c7edde40610a90681f54380f63598b8cb9
Sai Pc <schand31@asu.edu>
2015-12-19 08:22:43 +0530

    learnt how blocks differ

--- complex_blocks.rb
+++ complex_blocks.rb
@@ -1,21 +1,36 @@
-def a_method
+def a_method(&a_block)
   x = 0
   sum = 0
+  rum = 0
   while x < 5
     # yield is a block call, auto finds and
     # calls the block defined for this method
     # no need for the reference or for the explicit call
+    rum = a_block.call(sum, x)
     sum = yield sum, x
-    puts sum
+    puts sum, rum
     x = x + 1
   end
   puts "end of the method"
 end
 
+# looks like each 'block' is called when you add additional
+# blocks, first difference from methods
 a_method do |sum, x|
   # this is a block
   sum + x
 end
+
+a_method do |sum, x|
+  # this is a block
+  sum + x * x
+end
+
+a_method do |sum, x|
+  # this is a block
+  sum - x
+end
+
 
 # still not sure about the difference
 # between this and the following
