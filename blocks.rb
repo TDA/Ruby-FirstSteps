337bb861bd001cd22eb4b1a170a93415bcf5df14
Sai Pc <schand31@asu.edu>
2015-12-19 08:10:18 +0530

    basic blocks and methods

--- blocks.rb
+++ blocks.rb
@@ -11,3 +11,22 @@ a_method do
   # this is apparently a block
   puts "in the block"
 end
+
+# still not sure about the difference
+# between this and the following
+
+def b_method
+  # this is apparently a method that takes in a block
+  puts "in the method"
+  # this call seems synchronous
+  b_block
+  # control returns to the method
+  puts "end of the method"
+end
+
+def b_block
+  # this is apparently a block
+  puts "in the block"
+end
+
+b_method
