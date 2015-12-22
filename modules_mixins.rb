dc5b2800b1ad10b308b00d8b623cb1399f99e26d
Sai Pc <schand31@asu.edu>
2015-12-22 23:11:49 +0530

    Modules basic version with corrections

--- modules_mixins.rb
+++ modules_mixins.rb
@@ -1,9 +1,14 @@
-class ModulesMixins
-
-end
-
+#needs forward declaration
+# m is small, so module, not Module
 module ExModule
   def exMethod
     puts "Example"
   end
 end
+
+class ModulesMixins
+  include ExModule
+end
+
+modules_eg = ModulesMixins.new
+modules_eg.exMethod
