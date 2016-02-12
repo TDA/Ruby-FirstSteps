0d564ef1b618f1ba32c882b20a13e95472e613b8
Sai Pc <schand31@asu.edu>
2016-02-12 11:57:39 -0700

    added scopes

--- methods.rb
+++ methods.rb
@@ -2,6 +2,11 @@ def calc_mileage(miles, gallons)
   miles * 1.0 / gallons
 end
 
+
+# def calc_mileage(miles, gallons, time)
+#
+# end
+
 puts calc_mileage(10, 6)
 puts calc_mileage(20, 7)
 puts calc_mileage(10, 2)
--- miller_indices.rb
+++ miller_indices.rb
@@ -1,7 +1,7 @@
 class MillerIndices
   print "x = "
-  x = gets.to_f
-  puts x
+  @x = gets.to_f
+  puts @x
   print "y = "
   y = gets.to_f
   puts y
@@ -9,9 +9,17 @@ class MillerIndices
   z = gets.to_f
   puts z
 
-  reci_x = 1.0 / x
+  reci_x = 1.0 / @x
   reci_y = 1.0 / y
   reci_z = 1.0 / z
 
   puts reci_x, reci_y, reci_z
+
+  def get_x
+    print @x
+  end
+
 end
+
+# puts x
+# p MillerIndices.new.get_x
