309579230871c34714453e7fcd3fcb8bad38fb09
Sai Chandramouli <schand31@asu.edu>
2017-04-29 13:03:19 -0700

    more strings stuff

--- strings.rb
+++ strings.rb
@@ -1,4 +1,10 @@
-str = "With ♥!"
+str = "with ♥!"
 print("My String's encoding: ", str.encoding.name)
 print("\nMy String's size: ", str.size)
 print("\nMy String's bytesize: ", str.bytesize)
+puts "\n", str.ascii_only?
+puts "\n", str.capitalize
+puts "".empty?, "".nil?
+
+# equivalent to rails' blank :D
+puts "".nil? || "".empty?
