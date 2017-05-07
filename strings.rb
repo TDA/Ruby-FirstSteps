dadccfd216a62af68111dc22971f36bf6f0c0c5b
Sai Chandramouli <schand31@asu.edu>
2017-05-07 13:43:17 -0700

    byte and char

--- strings.rb
+++ strings.rb
@@ -48,7 +48,24 @@ serial_average('002-10.00-20.00')
 
 # string changes in ruby 1.9
 money = "¥1000"
+puts "Each byte"
 puts money.each_byte {|x| p x} # first char represented by two bytes
+
+puts "Each char"
 puts money.each_char {|x| p x} # prints each character
+
+puts "Each codepoint"
 puts money.each_codepoint {|x| p x} # prints each codepoint
+
+puts "Each line"
 puts money.each_line {|x| p x} # prints each line, for multiline strings
+
+def count_multibyte_char(strings)
+  count = 0
+  strings.each_char do |x|
+    count += 1 if x.bytesize > 1
+  end
+  count
+end
+
+puts count_multibyte_char('¥1000')
